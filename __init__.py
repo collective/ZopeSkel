c446dc7fcef415642988616189c6cd6a58c4bc6b
nouri <nouri@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2006-06-07 19:23:25 +0000

    * Changed plone_core so that it uses Cheetah templates now, which   allow for more flexible templates.
    * Added variable 'pythonproduts' which will, if set to True, render
      one more installation instruction in INSTALL.txt and
      ``<five:registerPackage package="." initialize=".initialize" />`` in
      the configure.zcml

--- __init__.py
+++ __init__.py
@@ -12,9 +12,13 @@ class PloneCore(templates.Template):
     _template_dir = 'templates/plone_core'
     summary = "A Plone Core project"
     required_templates = []
+    use_cheetah = True
     vars = [
         var('namespace_package', 'Namespace package', default='plone'),
-        var('package', 'The package contained namespace package (like i18n)'),
+        var('package', 'The package contained namespace package (like i18n)',
+            default='example'),
+        var('pythonproducts', 'Are you making a productsless Zope 2 Product?',
+            default=False),
         var('version', 'Version', default='0.1'),
         var('description', 'One-line description of the package'),
         var('long_description', 'Multi-line description (in reST)'),
