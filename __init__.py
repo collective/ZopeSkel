05b78a36e7dd1bc65e5bafe4a061c55a5d99d1be
optilude <optilude@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2007-01-27 23:09:36 +0000

    Add template for creating a buildout. This won't quite work yet, because the plone eggs are not released to anywhere; once they are in the cheese-shop, this would work. Otherwise, they could be listed from somewhere else, if a page of links were added to find-links in buildout.cfg_tmpl.

--- __init__.py
+++ __init__.py
@@ -101,3 +101,28 @@ class Plone2Theme(Namespace):
             'Name of the skin selection the new one will be copied from',
             default='Plone Default'),
         ] + vars[2:]
+
+class Plone3Buildout(templates.Template):
+    _template_dir = 'templates/plone3_buildout'
+    summary = "A buildout for Plone 3 projects"
+    required_templates = []
+    use_cheetah = True
+    
+    vars = [
+        var('zope2_install', 'Path to Zope 2 installation; leave blank to fetch one', default=''),
+        var('zope_user', 'Zope root admin user', default='admin'),
+        var('zope_password', 'Zope root admin password'),
+        var('debug_mode', 'Should debug mode be "on" or "off"?', default='off'),
+        var('verbose_security', 'Should verbose security be "on" or "off"?', default='off'),
+        ]
+        
+    def post(self, command, output_dir, vars):
+        print "-----------------------------------------------------------"
+        print "Generation finished"
+        print "You probably want to run python bootstrap.py and then edit"
+        print "buildout.cfg before running bin/buildout -v"
+        print 
+        print "See README.txt for details"
+        print "-----------------------------------------------------------"
+        
+    
