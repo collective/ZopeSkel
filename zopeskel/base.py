cc9a379ef3b9b8e00c6183c6fcc33de85b9f63f2
dokai <dokai@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-01-22 15:54:31 +0000

    Implemented support for adding the appropriate license classifier for a group of popular licenses.

--- zopeskel/base.py
+++ zopeskel/base.py
@@ -4,6 +4,22 @@ from paste.script.templates import BasicPackage
 
 var = templates.var
 
+LICENSE_CATEGORIES = {
+    'DFSG' : 'License :: DFSG approved',
+    'EFS' : 'License :: Eiffel Forum License (EFL)',
+    'NPL' : 'License :: Netscape Public License (NPL)',
+    'ASL' : 'License :: OSI Approved :: Apache Software License',
+    'BSD' : 'License :: OSI Approved :: BSD License',
+    'FDL' : 'License :: OSI Approved :: GNU Free Documentation License (FDL)',
+    'GPL' : 'License :: OSI Approved :: GNU General Public License (GPL)',
+    'LGPL' : 'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'MIT' : 'License :: OSI Approved :: MIT License',
+    'MPL' : 'License :: OSI Approved :: Mozilla Public License 1.0 (MPL)',
+    'MPL11' : 'License :: OSI Approved :: Mozilla Public License 1.1 (MPL 1.1)',
+    'QPL' : 'License :: OSI Approved :: Qt Public License (QPL)',
+    'ZPL' : 'License :: OSI Approved :: Zope Public License',
+    }
+
 def get_var(vars, name):
     for var in vars:
         if var.name == name:
