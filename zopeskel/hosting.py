e1d7171256cba14ef49ec3b57787d13abb7d17bc
wichert <wichert@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2008-04-22 10:49:10 +0000

    Update for Plone 3.1 release

--- zopeskel/hosting.py
+++ zopeskel/hosting.py
@@ -28,7 +28,7 @@ class StandardHosting(BaseTemplate):
             var("zeo_port", "ZEO port number", default=8100),
             var("proxy_port", "Proxy port number (optional)"),
             var("plone", "Plone version (2.5, 2.5.1, 3.0, 3.0.1, etc.)",
-                default="3.0.6"),
+                default="3.1"),
             ]
 
     def _buildout(self, output_dir):
