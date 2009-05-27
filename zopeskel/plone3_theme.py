e507d93ec42dfcccc38549860fc38b52c26eff29
maurits <maurits@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2009-05-27 20:23:19 +0000

    In the plone3_theme say that we are creating a theme for Plone 3, not specifically for 3.0.

--- zopeskel/plone3_theme.py
+++ zopeskel/plone3_theme.py
@@ -6,14 +6,14 @@ from zopeskel.base import get_var
 
 class Plone3Theme(Plone25Theme):
     _template_dir = 'templates/plone3_theme'
-    summary = "A Theme for Plone 3.0"
+    summary = "A Theme for Plone 3"
     required_templates = ['plone']
     use_cheetah = True
 
     vars = copy.deepcopy(Plone25Theme.vars)
     get_var(vars, 'namespace_package').default = 'plonetheme'
     get_var(vars, 'namespace_package').description = 'Namespace package (like plonetheme)'
-    get_var(vars, 'description').default = 'An installable theme for Plone 3.0'
+    get_var(vars, 'description').default = 'An installable theme for Plone 3'
 
     def pre(self, command, output_dir, vars):
         vars['timestamp'] = datetime.date.today().strftime("%Y%m%d")
