06611af4c0d508a72723e49fff7f9a73c670449b
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-19 20:10:39 +0000

    improving coverage of the zopeskel script.  The doctest is cheesy, but will at least help point out changes in the output of the script for now.
    I'd love to find a good way to actually interact with the paster create command while testing so we could test the inline help system and variable validation and so on.

--- zopeskel/tests/test_zopeskel_script.py
+++ zopeskel/tests/test_zopeskel_script.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 import unittest
+import sys
 
-from zopeskel.zopeskel_script import checkdots
+from zopeskel.zopeskel_script import checkdots, process_args, run
+from zopeskel.tests.test_zopeskeldocs import read_sh
 
 class test_zopeskel(unittest.TestCase):
     """Tests for ZopeSkel script.
@@ -30,7 +32,63 @@ class test_zopeskel(unittest.TestCase):
         self.assertRaises(ValueError, checkdots, t, "two.dots.but not legal")
 
         checkdots(t, "two.dots.legal")
+        
+    def test_process_args(self):
+        """Ensure that process_args correctly processes command-line arguments"""
+        oldargv = sys.argv
+        
+        sys.argv = ['zopskel']
+        self.assertRaises(SyntaxError, process_args)
+        
+        sys.argv.append('archetype')
+        processed = process_args()
+        self.failUnlessEqual(processed[0], 'archetype')
+        self.failIf(processed[1])
+        self.failIf(processed[2])
+        
+        sys.argv.append('my.project')
+        processed = process_args()
+        self.failUnlessEqual(processed[0], 'archetype')
+        self.failUnlessEqual(processed[1], 'my.project')
+        self.failIf(processed[2])
+        
+        sys.argv.append('--bob=kate')
+        processed = process_args()
+        self.failUnlessEqual(processed[0], 'archetype')
+        self.failUnlessEqual(processed[1], 'my.project')
+        self.failUnlessEqual(processed[2]['--bob'], 'kate')
 
+        # process_args will allow us to skip the project name argument
+        sys.argv.pop(2)
+        processed = process_args()
+        self.failUnlessEqual(processed[0], 'archetype')
+        self.failIf(processed[1])
+        self.failUnlessEqual(processed[2]['--bob'], 'kate')
+        
+        # providing arguments in '-name val' form is _not_ allowed
+        sys.argv = ['zopeskel', 'archetype', 'my.project', '-bob', 'kate']
+        self.assertRaises(SyntaxError, process_args)
+        
+        # the --svn-repository argument is _not_ allowed in any form
+        sys.argv = sys.argv[:3] + ['--svn-repository=svn://svn.junk.org/svn/blah']
+        self.assertRaises(SyntaxError, process_args)
+        
+        sys.argv[3] = 'svn-repository=svn://svn.junk.org/svn/blah'
+        self.assertRaises(SyntaxError, process_args)
+        
+        # providing args in a '-name val' format is not supported
+        sys.argv = sys.argv[:3] + ['bob', 'kate']
+        self.assertRaises(SyntaxError, process_args)
+        
+        sys.argv = oldargv
+
+    def test_script_errors(self):
+        """Verify that the run method catches errors correctly"""
+        
+        # non-existent templates are not caught until in 'run'
+        output = read_sh('zopeskel no-template my.package')
+        self.failUnless('ERROR: No such template' in output)
+        
 
 def test_suite():
     suite = unittest.TestSuite([
