be17840985bd3f4bbc48567c3492502c821ee2f8
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2010-03-22 19:28:04 +0000

    added code to support registering a generic setup profile in plone template packages
    refs:
     *  http://plone.org/products/zopeskel/issues/38
    
    and some other old tickets for the plone template.

--- zopeskel/plone.py
+++ zopeskel/plone.py
@@ -2,7 +2,8 @@ import copy
 
 from zopeskel.basic_zope import BasicZope
 from zopeskel.base import get_var
-from zopeskel.base import var
+from zopeskel.base import var, EASY, EXPERT
+from zopeskel.vars import BooleanVar
 
 class Plone(BasicZope):
     _template_dir = 'templates/plone'
@@ -19,6 +20,22 @@ To create a Plone project with a name like 'plone.app.myproject'
     use_local_commands = True
     use_cheetah = True
     vars = copy.deepcopy(BasicZope.vars)
+    vars.insert(5, BooleanVar(
+        'add_profile',
+        title='Register Profile',
+        description='Should this package register a GS Profile',
+        modes=(EASY, EXPERT),
+        default=False,
+        help="""
+If your package has need of a Generic Setup profile, set this value to 'True'.  
+
+Having a Generic Setup profile registered makes your package 'installable'
+using the ZMI portal_quickinstaller or Plone's 'Add/Remove Products' control
+panel.  This allows any portions of your package that require Generic
+Setup--such as portlets, content types, actions and so on--to be
+properly installed.
+"""
+    ))
     get_var(vars, 'namespace_package').default = 'plone'
     get_var(vars, 'package').default = 'example'
 
