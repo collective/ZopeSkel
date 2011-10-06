7c2c036cc047b588ede9a8c3c469b222ac2f88c4
cewing <cewing@db7f04ef-aaf3-0310-a811-c281ed44c4ad>
2011-10-06 21:46:05 +0000

    update plone default version for buildouts to 3.3.6

--- zopeskel/abstract_buildout.py
+++ zopeskel/abstract_buildout.py
@@ -8,7 +8,7 @@ VAR_PLONEVER = StringVar(
     'plone_version',
     title='Plone Version',
     description='Plone version # to install',
-    default='3.3.5',
+    default='3.3.6',
     modes=(EASY,EXPERT),
     page='Main',
     help="""
