3cc0e5ded630d005fbed556ab833304799d05ff9
cewing <cris@crisewing.com>
2012-06-12 20:09:12 -0700

    Subclass the main templer Runner class so we can override the show_version method.

--- control_script.py
+++ control_script.py
@@ -1,8 +1,24 @@
+import sys
+
 from templer.core.control_script import Runner
 from templer.core.control_script import run as templer_run
 
 
-zopeskel_runner = Runner(name='zopeskel', versions=['ZopeSkel', ])
+class ZopeSkelRunner(Runner):
+    """subclass templer runner to override show_version
+    """
+
+    def show_version(self):
+        """show installed version of packages listed in self.versions
+
+        method must exit by raising error or calling sys.exit
+        """
+        version_info = self._get_version_info()
+        print self._format_version_info(version_info)
+        sys.exit(0)
+
+
+zopeskel_runner = ZopeSkelRunner(name='zopeskel', versions=['ZopeSkel', ])
 
 
 def run():
