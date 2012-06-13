3c213c2af11c7f99f7f8582bc668dcef44873de2
cewing <cris@crisewing.com>
2012-06-12 18:24:57 -0700

    Override the templer command script with a special one that references 'zopeskel'.  The intention is to avoid user confusion on the part of users who have installed zopeskel and are not expecting to see references to 'templer'.
    refs Issue #2

--- control_script.py
+++ control_script.py
@@ -1,22 +1,9 @@
+from templer.core.control_script import Runner
 from templer.core.control_script import run as templer_run
-from templer.core.control_script import usage as templer_usage
-from templer.core.control_script import show_help as templer_show_help
-from templer.core.control_script import show_version as templer_show_version
-
-SCRIPT_NAME = "zopeskel"
-
-VERSION_PACKAGES = ['ZopeSkel']
 
 
-def show_version():
-    import pdb; pdb.set_trace()
-    templer_show_help()
+zopeskel_runner = Runner(name='zopeskel', versions=['ZopeSkel', ])
 
-def usage():
-    templer_usage()
-
-def show_help():
-    templer_show_help()
 
 def run():
-    templer_run()
+    templer_run(zopeskel_runner)
